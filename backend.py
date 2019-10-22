b985df7e5a93c071fa10db0e819b05ebeef3dc9c
Naklecha <nishant.aklecha@gmail.com>
2019-10-22 15:03:24 +0530

    Added buy method

--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request, session 
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+import requests
+
+PATH = "http://127.0.0.1:5000/"
 
 # ------------------------------------------------------
 
@@ -19,10 +22,12 @@ land_record = db['land_record']
 
 # ------------------------------------------------------
 
-class getxy(Resource):
+class xy(Resource):
     def get(self):
         req = eval(request.data)
-        try: # region - 1,2,3,4 clockwise
+        print(req)
+        try: # region - 0,1,2,3 clockwise 
+            # expecting {'region': 1, 'x': 1, 'y': 1}
             find = land_record.find_one({"region": req["region"],"x": req["x"],"y": req["y"]})
             find.pop('_id')
             print(find)
@@ -31,7 +36,7 @@ class getxy(Resource):
             return {},400
     def post(self):
         req = eval(request.data)
-        try:
+        try: # expecting req to be like {'region': 1, 'x': 1, 'y': 1, 'nickname': '_empty', 'price': 0, 'status': 'no owner'}
             myquery = {"region": req["region"],"x": req["x"],"y": req["y"]}
             newvalues ={ "$set":  { "nickname": req["nickname"], "status":req["status"],"price":req["price"]}}
             land_record.update_one(myquery, newvalues)
@@ -42,7 +47,23 @@ class getxy(Resource):
         except:
             return {},400
 
-api.add_resource(getxy, '/getxy')
+api.add_resource(xy, '/xy')
+
+# ------------------------------------------------------
+
+class buy(Resource):
+    def post(self):
+            req = eval(request.data) # expecting {'region': 1, 'x': 1, 'y': 1, 'nickname': '_empty', 'price': 100}
+            print(req)
+            getxy = requests.get(PATH+"xy",json = {"region": req["region"],"x": req["x"],"y": req["y"]})
+            getxy = eval(getxy._content)
+            print(getxy)
+            if(getxy['price']<=req['price']):
+                postxy = requests.post(PATH+"xy",json = {"region": req["region"],"x": req["x"],"y": req["y"], 'nickname': req['nickname'], 'price': req['price'], 'status': 'not for sale'})          
+                return eval(postxy._content)
+            else:
+                return "Not enough funds", 400
+api.add_resource(buy, '/buy')
 
 # ------------------------------------------------------
 
